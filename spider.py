9fe9bb1bc4799a19f8b89d410e20762183867895
keeplearning01 <kenny101138@gmail.com>
2023-02-24 21:32:14 +0800

    nothing new

--- spider.py
+++ spider.py
@@ -3,51 +3,48 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 from bs4 import BeautifulSoup as Soup 
-def find(search_name):
-    driver = webdriver.Chrome("./flasktest/chromedriver.exe")
-    driver.get('https://www.instagram.com/')
-    time.sleep(2)
-    email=driver.find_element(By.NAME, "username")
-    password=driver.find_element(By.NAME, "password")
-    email.send_keys('testing_ac_c')
-    password.send_keys('testingacc')
-    button=driver.find_element(By.CLASS_NAME,"_acan._acap._acas._aj1-")
-    button.click()
-    time.sleep(2)
-    search_name=input("account:")
-    driver.get("https://www.instagram.com/"+search_name)
-    post_url = []
-    for i in range(5):
-        driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
-        time.sleep(1)
-        html = driver.page_source
-        soup = Soup(html, 'lxml')
+driver = webdriver.Chrome("./flasktest/chromedriver.exe")
+driver.get('https://www.instagram.com/')
+time.sleep(2)
+email=driver.find_element(By.NAME, "username")
+password=driver.find_element(By.NAME, "password")
+email.send_keys('testing_ac_c')
+password.send_keys('testingacc')
+button=driver.find_element(By.CLASS_NAME,"_acan._acap._acas._aj1-")
+button.click()
+time.sleep(2)
+search_name=input("account:")
+driver.get("https://www.instagram.com/"+search_name)
+post_url = []
+for i in range(5):
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+    time.sleep(1)
+    html = driver.page_source
+    soup = Soup(html, 'lxml')
         # 尋找所有的貼文連結
-        for elem in soup.select('article div div div div a'):
+    for elem in soup.select('article div div div div a'):
             # 如果新獲得的貼文連結不在列表裡，則加入
-            if elem['href'] not in post_url:
-                post_url.append(elem['href'])
-        time.sleep(2) # 等待網頁加載
+        if elem['href'] not in post_url:
+            post_url.append(elem['href'])
+    time.sleep(2) # 等待網頁加載
+check=0
+for url in post_url :
     check=0
-    for url in post_url :
-        check=0
-        find=False
-        while not find:
+    find=False
+    while not find:
+        try:
+            post_elem = driver.find_element(By.XPATH, '//a[@href="'+str(url)+'"]')
+            action = ActionChains(driver)
+            action.move_to_element(post_elem).perform()
+            driver.find_elements(By.CLASS_NAME,'_ac2d')
             try:
-                post_elem = driver.find_element(By.XPATH, '//a[@href="'+str(url)+'"]')
-                action = ActionChains(driver)
-                action.move_to_element(post_elem).perform()
-                driver.find_elements(By.CLASS_NAME,'_ac2d')
-                try:
-                    n_like_elem =driver.find_elements(By.CLASS_NAME,'_aacl._aacp._adda._aad3._aad6._aade')
-                    n_like = n_like_elem[0].text
-                    find=True
-                except:
-                    break
-            
+                n_like_elem =driver.find_elements(By.CLASS_NAME,'_aacl._aacp._adda._aad3._aad6._aade')
+                n_like = n_like_elem[0].text
+                find=True
             except:
-                scroll = 'window.scrollBy(0,-250)'
-                driver.execute_script(scroll)
-                continue
-    return n_like
-    
+                break
+            
+        except:
+            scroll = 'window.scrollBy(0,-250)'
+            driver.execute_script(scroll)
+            continue    
